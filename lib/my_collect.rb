f9bfa44aab9eb24d11fda3116354d95345bdadb9
Tarric Sookdeo <tarricsookdeo@outlook.com>
2019-10-12 21:52:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  # While loop counter init
+  i = 0
+  # Stores values to be returned
+  new_array = []
+  
+  while i != array.length do
+    new_array.push(yield(array[i]))
+    i += 1
+  end
+  
+  return new_array
+end
